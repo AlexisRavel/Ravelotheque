a85549f49a6ef714c90d272d719f0bdcc49a35b9
AlexisRavel <alexis.ravel@outlook.fr>
2023-10-02 16:58:05 +0200

    Add models

--- backend/admin.py
+++ backend/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Author, Editor, Collection, Book
 
 # Register your models here.
+admin.site.register(Author)
+admin.site.register(Editor)
+admin.site.register(Collection)
+admin.site.register(Book)
--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
 # Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=32, null=False)
+    birthDate = models.DateField(null=False)
+    deathDate = models.DateField(null=True)
+    biography = models.TextField(editable=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Editor(models.Model):
+    name = models.CharField(max_length=16, null=False)
+
+    def __str__(self):
+        return self.name
+
+class Collection(models.Model):
+    name = models.CharField(max_length=16, null=False)
+    editor = models.ForeignKey(Editor, on_delete=models.PROTECT, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    GENRE = [
+        ("SF", "Science-Fiction"),
+        ("PH", "Philosophie"),
+        ("RO", "Roman"),
+    ]
+
+    title = models.CharField(max_length=40, null=False)
+    author = models.ForeignKey(Author, on_delete=models.PROTECT, null=False)
+    editor = models.ForeignKey(Editor, on_delete=models.PROTECT, null=True)
+    collection = models.ForeignKey(Collection, on_delete=models.PROTECT, null=True)
+    genre = models.CharField(max_length=24, choices=GENRE, null=True)
+
+    def __str__(self):
+        return self.title
